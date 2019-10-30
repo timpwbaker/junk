03c1d9290e3c47d102cf3d28babad386156a72e5
timpwbaker <timpwbaker@gmail.com>
2019-10-30 18:06:38 +0000

    working 5, times out

---
+++ algos/5.longest-palindromic-substring.rb
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=5 lang=ruby
+#
+# [5] Longest Palindromic Substring
+#
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+#
+# algorithms
+# Medium (28.16%)
+# Total Accepted:    697.6K
+# Total Submissions: 2.5M
+# Testcase Example:  '"babad"'
+#
+# Given a string s, find the longest palindromic substring in s. You may assume
+# that the maximum length of s is 1000.
+#
+# Example 1:
+#
+#
+# Input: "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+#
+#
+# Example 2:
+#
+#
+# Input: "cbbd"
+# Output: "bb"
+#
+#
+#
+# @param {String} s
+# @return {String}
+require "pry"
+def longest_palindrome(s)
+  return "" if s == ""
+  max_length = s.length/2
+  longest = s[0]
+
+  s.chars.each_with_index do |c, i|
+    # Either
+    #
+    # n to the left of i == reverse of n to the right of i
+    #
+    # or
+    #
+    # n - 1 to the left of i (excluding i) == n to the right of i (including
+    # i)
+    (longest.length/2..i).each do |n|
+      if s[i-n..i-1] == s[i+1..i+n].reverse
+        palindrome = s[i-n..i+n]
+        longest = palindrome if palindrome.length > longest.length
+      end
+
+      if s[i-n..i-1] == s[i..i+n-1].reverse
+        palindrome = s[i-n..i+n-1]
+        longest = palindrome if palindrome.length > longest.length
+      end
+    end
+  end
+  longest
+end
+
+puts longest_palindrome("abb")
+puts longest_palindrome("aaaa")
+puts longest_palindrome("bbaabb")
+puts longest_palindrome("bbacabb")
